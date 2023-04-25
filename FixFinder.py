dafbca2a37534fbd69303491841eebbb0c3a7bf4
0xHamzDev <131713310+0xHamzDev@users.noreply.github.com>
2023-04-25 08:22:47 -0500

    Create FixFinder.py

---
+++ FixFinder.py
@@ -0,0 +1,11 @@
+import subprocss
+import os
+
+def main():
+    print("FixFinder program is running!")
+    
+if __name__ == "__main__":
+    main()
+
+git add FixFinder.py
+git commit -m "Initial commit"
