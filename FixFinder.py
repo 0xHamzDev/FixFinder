15f4ad9d53a671fde02d6387e851b4907c6a04d3
0xHamzDev <131713310+0xHamzDev@users.noreply.github.com>
2023-04-29 02:35:20 -0500

    Update FixFinder.py

--- FixFinder.py
+++ FixFinder.py
@@ -13,7 +13,18 @@ def main():
     parser.add_argument("-s", "--sev", default="medium",
                         choices=["low", "medium", "high"],
                         help="Severity level of vulnerabilities to scan for")
+    parser.add_argument("-local", action="store_true",
+                        help="Automatically use local IP address for the scan")
     args = parser.parse_args()
+    
+    # Use local IP address if -local flag is provided
+    if args.local:
+        local_ip = socket.gethostbyname(socket.gethostname())
+        print(f"Scanning {local_ip}...")
+        args.targets = [local_ip]
+    elif not args.targets:
+        print("Error: no targets specified")
+        exit()
 
     # Validate input
     targets = []
@@ -167,3 +178,79 @@ def generate_report(scan_results):
             print("No misconfigurations found.")
         print("=" * 100)
 
+def scan_with_nmap(target, port_range, severity):
+    # If "-local" flag is passed, detect the local IP address
+    if target == '-local':
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        target = s.getsockname()[0]
+        s.close()
+
+    # Use nmap to scan for vulnerabilities and collect data in a dictionary
+    nm = nmap.PortScanner()
+    nm.scan(hosts=target, ports=port_range, arguments='-sV -sC -O --script vuln')
+    vulnerabilities_data = {
+        "target": target,
+        "port_range": port_range,
+        "severity": severity,
+        "vulnerabilities": {}
+    }
+
+    # Filter vulnerabilities based on severity
+    for host in nm.all_hosts():
+        for proto in nm[host].all_protocols():
+            lport = nm[host][proto].keys()
+            for port in lport:
+                port_info = nm[host][proto][port]
+                for vuln in port_info["script_results"]["vuln"]:
+                    if vuln["severity"] == severity:
+                        if host not in vulnerabilities_data["vulnerabilities"]:
+                            vulnerabilities_data["vulnerabilities"][host] = {}
+                        if proto not in vulnerabilities_data["vulnerabilities"][host]:
+                            vulnerabilities_data["vulnerabilities"][host][proto] = {}
+                        vulnerabilities_data["vulnerabilities"][host][proto][port] = {
+                            "name": vuln["id"],
+                            "description": vuln["output"],
+                            "severity": vuln["severity"],
+                            "cve": vuln["cve"] if "cve" in vuln else ""
+                        }
+
+    return vulnerabilities_data
+
+def scan_with_openvas(target, severity):
+    # Use an external tool like OpenVAS to scan for vulnerabilities
+    # and collect data in a dictionary
+    vuln_data = {
+        "target": target,
+        "vulnerabilities": [
+            {
+                "name": "CVE-2020-1234",
+                "severity": "High",
+                "description": "A remote attacker could exploit this vulnerability...",
+                "solution": "Apply the appropriate patch or upgrade.",
+                "references": [
+                    "https://nvd.nist.gov/vuln/detail/CVE-2020-1234",
+                    "https://example.com/security-advisory/cve-2020-1234",
+                ],
+            },
+            {
+                "name": "CVE-2021-5678",
+                "severity": "Medium",
+                "description": "An attacker could exploit this vulnerability...",
+                "solution": "Update to the latest version or apply the available patches.",
+                "references": [
+                    "https://nvd.nist.gov/vuln/detail/CVE-2021-5678",
+                    "https://example.com/security-advisory/cve-2021-5678",
+                ],
+            },
+            # More vulnerabilities...
+        ],
+    }
+
+    # Filter vulnerabilities based on severity level
+    vulnerabilities = []
+    for vuln in vuln_data["vulnerabilities"]:
+        if vuln["severity"] == severity:
+            vulnerabilities.append(vuln)
+
+    return vulnerabilities
